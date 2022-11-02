86b078983ae2862dd533052cdc94cc5c1812429c
Kelvin <kelvinnicolau7@gmail.com>
2022-11-02 19:07:05 -0300

    Iniciando aplicação WEB

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,15 @@ import express from 'express';
 
 const app = express();
 
-// wwww.minhaapi.com/adc
+// wwww.minhaapi.com/ads
 
 app.get('/ads', (request, response)=>{
     return response.json([
         { id: 1, name: 'Anuncio 1' },
         { id: 2, name: 'Anuncio 2' },
         { id: 3, name: 'Anuncio 3' },
+        { id: 4, name: 'Anuncio 4' },
+        { id: 5, name: 'Anuncio 5' },
     ])
 })
 
---
+++ web/src/App.tsx
@@ -0,0 +1,20 @@
+function Button() {
+  return (
+    <button>
+      Send
+    </button>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+    </div>
+  )
+}
+
+
+export default App
