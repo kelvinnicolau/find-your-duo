e997a4a40ebf144816f7c45d66b864efc2875090
Kelvin <kelvinnicolau7@gmail.com>
2022-11-19 23:24:40 -0300

    criacao de rotas e definicao da aplicacao

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,27 @@ import express from 'express';
 
 const app = express();
 
+// HTTPS methods / API RESTful / HTTP Codes
+
+// GET, POST, PUT, PATCH, DELETE 
+
+// Query: ...
+// Route: ...
+// Body: ...
+
 // wwww.minhaapi.com/ads
 
-app.get('/ads', (request, response)=>{
+app.get('/games', (request, response) => {
+    return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response)=>{
+    // const gameId = request.params.id;
+
     return response.json([
         { id: 1, name: 'Anuncio 1' },
         { id: 2, name: 'Anuncio 2' },
@@ -12,6 +30,12 @@ app.get('/ads', (request, response)=>{
         { id: 4, name: 'Anuncio 4' },
         { id: 5, name: 'Anuncio 5' },
     ])
+})
+
+app.get('/ads/:id/discord', (request, response)=>{
+    // const gameId = request.params.id;
+
+    return response.json([])
 })
 
 app.listen(3333);
