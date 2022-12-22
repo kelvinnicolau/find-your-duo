c1b564a81b171f7c0f01d59252b61d20c73901ce
Kelvin <kelvinnicolau7@gmail.com>
2022-12-22 01:32:51 -0300

    adc compo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { MagnifyingGlassPlus } from 'phosphor-react';
+import * as Dialog from '@radix-ui/react-dialog';
 
 import './styles/main.css';
 
@@ -6,6 +6,7 @@ import { useState, useEffect } from 'react';
 
 import logoImg from './assets/logo-nlw-esports.svg'
 import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
 
 interface Game {
   id: string;
@@ -46,24 +47,9 @@ function App() {
             adsCount={game._count.ads} />
           )
         })}
-
-
       </div>
 
-      <div className='pt-1 bg-nlw-gradient self-stretch mt-8 rounded-lg overflow-hidden'>
-        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
-          <div>
-            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
-          </div>
-
-          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
-            <MagnifyingGlassPlus size={24}/>
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
-
+      <CreateAdBanner />
     </div>
   )
 }
--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -6,8 +6,6 @@ interface GameBannerProps {
 
 export function GameBanner (props: GameBannerProps) {
 
-    
-
     return (
         <a href="" className='relative rounded-lg overflow-hidden'>
           <img src={props.bannerUrl} alt="" />
